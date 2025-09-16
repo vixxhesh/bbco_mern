7c7da402553620938a3b162a8af42c5a57141b90
Vishesh <80572483+vixxhesh@users.noreply.github.com>
2025-09-16 20:53:32 +0530

    Delete server directory

--- server/models/userModel.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-const userSchema = new mongoose.Schema({});
-
-const userModel = mongoose.model.user || mongoose.model("user", userSchema);
-
-export default userModel;
